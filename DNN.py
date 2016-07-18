b5bd69d402a8fcd38cac530d3dc8052584a8ad50
Luis Gonzalo Sanchez Giraldo <luisitobarcito@gmail.com>
2016-07-18 16:10:30 -0400

    initial commit

---
+++ DNN.py
@@ -0,0 +1,219 @@
+""" 
+DNN is a little python module to demonstrate 
+the basic elements of a deep neural network 
+in action.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pointwise_activations import func_list
+from loss_functions import loss_list
+
+class Data(object):
+    """
+    Data class is a manaer class that takes care of the minibatch management
+    """
+    batch_iter = None # batch iterator
+    data_size = None
+    batch_idx = None
+    crt_idx = None
+    n_batches = None
+    shuffle = None
+    
+    def __init__(self, data, batch_size=None, shuffle=True):
+        self.data = data
+        self.data_size = data.shape[0]
+        self.shuffle = shuffle
+        if batch_size is None:
+            self.batch_size = self.data_size
+            self.batch_iter = None
+            self.batch_idx = None
+            self.shuffle = False
+        else:
+            assert self.data_size >= batch_size, 'batch_size exeeds number of data samples'
+            self.batch_size = batch_size
+            self.batch_iter = 0
+        self.n_batches = self.data_size / self.batch_size
+        leftovers = self.data_size % self.batch_size
+                
+    def getBatch(self):
+        if self.batch_iter is None:
+            return self.data
+  
+        if self.batch_iter == 0:
+            ## shuffle data every epoch
+            if self.shuffle is True:
+                self.batch_idx = np.random.permutation(self.data_size) 
+            else:
+                self.batch_idx = np.arange(self.data_size)
+            self.batch_idx = np.reshape(self.batch_idx, (self.n_batches, -1))
+        self.crt_idx = self.batch_idx[self.batch_iter, :]
+        batch = self.data[self.crt_idx, ::]
+        self.batch_iter += 1
+        if self.batch_iter == self.n_batches:
+            self.batch_iter = 0
+        return batch
+
+    def getDataAsIn(self, ref_data_object=None):
+        if ref_data_object.crt_idx is not None:
+            return self.data[ref_data_object.crt_idx, ::]
+        else:
+            return self.data
+    
+
+class Layer(object):
+    """
+    Layer class implements a uniform composition of affine map followed by 
+    point-wise nonlinearity
+    Input: 
+    -- n_in: number of inputs
+    -- n_out: number of outputs (numer of units)
+    -- activation: point-wise nonlinearity. logistic, tanh, ReLU, abs, 
+       square, halfsquare
+    -- bias: use bias (default True)
+    """
+
+    X0 = None
+    X1 = None
+    Z = None
+    D0 = None
+    D1 = None
+    W = None
+    b = None
+    Delta_W = None
+    Delta_b = None
+    g = None
+    g_prime = None
+    n_in = None
+    n_out = None
+
+    def __init__(self, n_in, n_out, activation):
+        assert n_in is not None and n_out is not None, "layer must hav valid inout output sizes"
+        self.n_in = n_in
+        self.n_out = n_out
+        self.W = np.random.normal(size=(self.n_in, self.n_out)) / np.sqrt(self.n_in)
+        self.b = np.zeros((1, self.n_out))
+        self.Delta_W = np.zeros((self.n_in, self.n_out))
+        self.Delta_b = np.zeros((1, self.n_out))
+        self.g = func_list[activation][0]
+        self.g_prime = func_list[activation][1]
+    
+        
+    def forward(self):
+        self.Z = np.dot(self.X0, self.W) + self.b
+        self.X1 = self.g(self.Z)
+    
+    def backward(self):
+        if self.D1 is None:
+            self.G = self.g_prime(self.Z)
+        else:
+            self.G = np.multiply(self.D1, self.g_prime(self.Z))
+        self.D0 = np.dot(self.G, self.W.transpose())
+
+    def updateParam(self, solver_func):
+        self.Delta_W, self.Delta_b = solver_func(self)
+        self.W += self.Delta_W
+        self.b += self.Delta_b
+
+class Net(object):
+    "docstring"
+    
+    n_layer = 0
+    layers = []
+    Xout = None
+    def __init__(self):
+        self.n_layer = 0
+        self.layers = []
+        self.Xout = None
+    
+    def addLayer(self, n_in=None, n_out=None, activation=None):
+        assert n_in is not None or self.n_layer > 0, "n_in must be specified for input layer"
+        assert n_out is not None, "n_out must be specified"
+        assert activation is not None, "activation must be specified"
+ 
+        if n_in is not None and self.n_layer > 0:
+            assert n_in == self.layers[-1].n_out, "n_in does not match with previous layer number of units"
+        elif self.n_layer > 0:
+            n_in = self.layers[-1].n_out
+      
+        self.layers += [Layer(n_in, n_out, activation)]
+        self.n_layer += 1
+ 
+    def forward(self, X):
+        X0 = X
+        for layer in self.layers:
+            layer.X0 = X0
+            layer.forward()
+            X0 = layer.X1
+        self.Xout = X0
+        
+    def backward(self, DeltaN):
+        Delta1 = DeltaN
+        for layer in self.layers[-1::-1]:
+            layer.D1 = Delta1
+            layer.backward()
+            Delta1 = layer.D0
+
+    def updateParam(self, solver_func=None):
+        if solver_func is None:
+            pass
+        else:
+            for layer in self.layers[-1::-1]:
+                layer.updateParam(solver_func)
+
+
+class NetTrainer(object):
+    "docstring"
+
+    max_iter = 0
+    solver_func = None
+    loss = None
+    lossPrime = None
+    net = None
+    print_interval = 100
+    batch_size = None
+
+    def __init__(self, net=None, loss_func='mse', batch_size=None, max_iter=1000, train_data=None, label_data=None, solver=None, print_interval=None):
+        self.net = net
+        self.batch_size = batch_size
+        self.solver_func = solver.solverFunc()
+        self.loss = loss_list[loss_func][0]
+        self.lossPrime = loss_list[loss_func][1]
+        self.max_iter = max_iter
+        self.data = Data(train_data, batch_size=self.batch_size)
+        self.labels = Data(label_data)
+        if print_interval is not None:
+            self.print_interval = print_interval
+        
+    def train(self, n_iter=None):
+        for iTr in range(self.max_iter):
+            Xin = self.data.getBatch()
+            T = self.labels.getDataAsIn(self.data)
+            self.net.forward(Xin)
+            objective = np.mean(self.loss(T, self.net.Xout), axis=0)
+            self.net.backward(self.lossPrime(T, self.net.Xout) / T.shape[0])
+            self.net.updateParam(self.solver_func)
+            if iTr % self.print_interval == 0:
+                print "Iteration %d, objective = %f" % (iTr,objective)
+
+class Solver(object):
+    "docstring"
+
+    lr_rate = None
+    rate_decay = None
+    momentum = None
+    solver = None
+    
+    def __init__(self, params):
+        for prm_name in params.keys():
+            setattr(self, prm_name, params[prm_name])
+
+    def solverFunc(self):
+        return getattr(self, self.solver)
+
+    def sgd(self, layer):
+        Delta_W = self.momentum * layer.Delta_W - self.lr_rate * np.dot(layer.X0.transpose(), layer.G) 
+        Delta_b = self.momentum * layer.Delta_b - self.lr_rate * np.sum(layer.G, axis=0)
+        return Delta_W, Delta_b
+
+
